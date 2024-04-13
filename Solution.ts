cd027da554f2ebe88a78911c5a06396b7de52390
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-04-13 09:54:34 +0200

    Create Solution.ts

---
+++ Solution.ts
@@ -0,0 +1,19 @@
+
+function findLucky(input: number[]): number {
+    const NOT_FOUND = -1;
+    const RANGE_OF_VALUES = [1, 500];
+
+    const frequency: number[] = new Array(RANGE_OF_VALUES[1] + 1).fill(0);
+    for (let value of input) {
+        ++frequency[value];
+    }
+
+    let maxLuckyInteger = NOT_FOUND;
+    for (let value of input) {
+        if (value === frequency[value] && maxLuckyInteger < value) {
+            maxLuckyInteger = value;
+        }
+    }
+
+    return maxLuckyInteger;
+};
